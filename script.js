38eb3508adc06f08a77bec2e30a9af444223f643
Zizo95 <zizo.brown.mbiko@gmail.com>
2024-05-01 11:08:56 +0200

    uupdated css

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const numbersEl = document.querySelector('.numbers');
 const ratingEl = document.querySelector(".rating-no");
+const sectionEl = document.querySelector("section");
+const firstSection = document.querySelector(".first");
 const buttonEl = document.querySelector("button");
 
 numbersEl.addEventListener("click", function(){
   numbersEl.style.background = "hsl(25, 97%, 53%)"
   numbersEl.style.color = "hsl(0, 0%, 100%)"
+})
+
+buttonEl.addEventListener("click",function(){
+  if (numbersEl == 1){
+ratingEl.innerHTML = "1";
+  }
+sectionEl.style.display = "block"
+firstSection.style.display = "none"
 })
