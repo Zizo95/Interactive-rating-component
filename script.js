3fd1d74bfc2f91b3bb215fa1760f1e2061eda316
Zizo95 <zizo.brown.mbiko@gmail.com>
2024-05-01 10:42:09 +0200

    updated css

---
+++ script.js
@@ -0,0 +1,8 @@
+const numbersEl = document.querySelector('.numbers');
+const ratingEl = document.querySelector(".rating-no");
+const buttonEl = document.querySelector("button");
+
+numbersEl.addEventListener("click", function(){
+  numbersEl.style.background = "hsl(25, 97%, 53%)"
+  numbersEl.style.color = "hsl(0, 0%, 100%)"
+})
